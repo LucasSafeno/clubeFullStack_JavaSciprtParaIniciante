556e270c4174bd94b45500fc343cef7a7e4a08cd
Lucas Safeno <lugnorio@gmail.com>
2025-06-04 11:12:38 -0300

    Curso de Javascript#02 - O que são e como nomear as variáveis

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//? Variaveis
+var firstName = 'Lucas';
+var _lastName = 'Tenório';
+var age = 30;
+var $year = 2025;
+
+console.log($year);
+console.log(_lastName);
+console.log(firstName);
+console.log(age);
